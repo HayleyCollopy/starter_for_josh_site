54392fe7a7729e11f00caa87b65314a2a4369015
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-03-18 18:07:28 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,29 @@
+<footer class="container-fluid">
+          <div class="container">
+              <div class="row">
+              <div class="col-md-6 text-center footer-content">
+                  <p>JOSH WHITKIN</p>
+                  <img src="images/linkedin.jpg" alt="facebook">
+                  <img src="images/twitter.jpg" alt="facebook">
+                  </div>
+                  <div class="col-md-6 text-center footer-content">
+                  <p>Terms and Conditions</p>
+                      <img src="images/wifi.jpg" alt="facebook">
+                      <img src="images/facebook.jpg" alt="facebook">
+                  </div>
+              
+              
+              
+              
+              </div>
+          </div>
+      </footer>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!--<script src="js/bootstrap.min.js"></script>-->
+  </body>
+</html>
+<?php wp_footer(); ?>
