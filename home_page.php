8c482389991c2c6f4c41566e917a30d85b9cc365
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-04-16 00:24:29 +0800

    Update home_page.php

--- home_page.php
+++ home_page.php
@@ -10,7 +10,7 @@ Template Name: Home Page
               
 
 <!--              placeholder for about me-->
-              
+              <h2><?php the_field('titleforaboutme'); ?></h2><!--  custom field for the aboutme content-->
     	
  <div class="blocker"></div>
             
