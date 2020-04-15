126b9b17b86a22914c54b5528e7a522a1058e96f
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-04-15 23:07:45 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,6 @@ wp_reset_query();?>
                   <h3 class="text-center article-title">LATEST ARTICLES</h3>
                   </div>
               <div class="col-md-4 article-cent">
-
 <?php $post_id = 16;
 $queried_post = get_post($post_id);?>
 <?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
@@ -43,19 +42,21 @@ $queried_post = get_post($post_id);?>
                   </div>
               
                <div class="col-md-4 article-cent">
-                    <img class="img-responsive" alt="model" src="images/a-model.jpg">
-                  <h4>A PORTRAIT</h4>
-                  <p class="date">12 December 2020</p>
-                  <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
-                  <p class="readmore">CONTINUE READING</p>
+<?php $post_id = 18;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?> 
                   </div>
                   
               <div class="col-md-4 article-cent">
-                    <img class="img-responsive" alt="gamesdesign" src="images/games-design.jpg">
-                  <h4>A PORTRAIT</h4>
-                  <p class="date">12 December 2020</p>
-                  <p>STS has never been shy of seeking new terrains of  investigation. More and  more  STS s cholars are  starting to explore and intervene in the arts.  This object of study  brings  new challenges and  opportunities that we want  to explore in this session.   We would  like to gather first of all simply  new kinds of  knowledge arising from  STS study  of the arts. </p>
-                  <p class="readmore">CONTINUE READING</p>
+<?php $post_id = 20;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?> 
                   </div>
               
               </div>
