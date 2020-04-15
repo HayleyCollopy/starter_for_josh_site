a289fcff173bfa4ba59a107129d40497b7ef9117
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-04-16 00:19:16 +0800

    Create home_page.php

---
+++ home_page.php
@@ -0,0 +1,46 @@
+<?php
+/*
+Template Name: Home Page
+*/
+?>
+<?php get_header(); ?>
+      <section class="container-fluid aboutbg text-center">
+      
+          <div class="container">
+              
+
+<!--              placeholder for about me-->
+              
+    	
+ <div class="blocker"></div>
+            
+      </div><!--  container-->
+    </section>
+
+      
+      
+      <section class="container-fluid articlebg">
+          <div class="container">
+              <div class="row">
+                  <div>
+                  <h3 class="text-center article-title">LATEST ARTICLES</h3>
+                  </div>
+              <div class="col-md-4 article-cent">
+<!--placeholder for col1-->                       
+
+                  </div>
+              
+               <div class="col-md-4 article-cent">
+<!--placeholder for col2-->    
+                  </div>
+                  
+              <div class="col-md-4 article-cent">
+<!--placeholder for col3-->    
+                  </div>
+              
+              </div>
+<!--              row-->
+          </div>
+<!--          container-->
+      </section>
+<?php get_footer(); ?>
